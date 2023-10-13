4a20614d7de23fe9134ab1b1fc0ed8e3ebc7b080
jschroevers <101391784+Jamiroschroevers@users.noreply.github.com>
2023-10-13 15:12:50 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,15 +1,4 @@
 <?php
-echo "test";
-echo "test1";
-echo "test2";
-echo "test3";
-echo "test4";
-echo "test5";
-echo "test6";
-echo "test10";
-echo "test11";
 echo "blue";
 echo "geel";
-echo "test10";
-echo "test11"
 ?>
